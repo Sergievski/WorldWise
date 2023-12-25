8962c39bd8492e5a8eba737f28ca0416b19a9511
Stas.S <stas.eco82@gmail.com>
2023-12-25 18:31:49 +0200

    Finishing the City View - Video 229

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -3,11 +3,14 @@ import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContex";
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
-  if (!cities.length)
+  if (!cities)
     return (
       <Message message="Add your first city by clicking on a city on the map" />
     );
